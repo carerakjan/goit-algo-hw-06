da1bbcdeb534b2608caa30f56010b25ee5637aa7
Bo_Hr <carerakjan@gmail.com>
2024-03-14 17:33:32 +0200

    spread the logic

--- main.py
+++ main.py
@@ -1,28 +1,8 @@
-from handlers import parse_input, add_contact, change_contact, show_phone, show_all
+from assistant import run_assistant
 
 
 def main():
-    contacts = {}
-    print("Welcome to the assistant bot!")
-    while True:
-        user_input = input("Enter a command: ")
-        command, *args = parse_input(user_input)
-
-        if command in ["close", "exit"]:
-            print("Good bye!")
-            break
-        elif command == "hello":
-            print("How can I help you?")
-        elif command == "add":
-            print(add_contact(args, contacts))
-        elif command == "change":
-            print(change_contact(args, contacts))
-        elif command == "phone":
-            print(show_phone(args, contacts))
-        elif command == "all":
-            print(show_all(contacts))
-        else:
-            print("Invalid command.")
+   run_assistant()
 
 
 if __name__ == "__main__":
